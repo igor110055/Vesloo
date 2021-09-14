24398e64ea766ab0e03b5d75ea18185248051865
Wesley <40546525+Vesloo@users.noreply.github.com>
2021-09-14 14:46:51 +0200

    Delete mixedmoviesarts directory

--- mixedmoviesarts/header/subnavbar.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="/mma/css/subnavbar.css">
-
-<div class="subnavbar">
-    <p class="title text">Mixed Movies Arts</p>
-    <p class="contact text">60 rue François 1er, Paris 75008 | </p> 
-    <p class="contact text">01 01 01 01 01</p> 
-</div>
